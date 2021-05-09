f9cbc4656987aefc977aa7d27b0210856e367400
Sara89850 <saraeliyahu2017@gmail.com>
2021-05-09 20:06:10 +0300

    seating -continue

--- src/app/components/seating-arrangement/seating-arrangement.component.ts
+++ src/app/components/seating-arrangement/seating-arrangement.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Guest } from 'src/app/model/guest.model';
 import { Table } from '../../model/tables.model';
-// import { SeatingArrangement } from '../../model/SeatingArrangement.model';
+import { SeatingArrangement } from '../../model/SeatingArrangement.model';
 import { SeatingService } from '../../service/SeatingService';
+import { GuestService } from '../../service/guest.service';
+import { ignoreElements } from 'rxjs/operators';
 
 @Component({
   selector: 'app-seating-arrangement',
@@ -13,19 +15,36 @@ import { SeatingService } from '../../service/SeatingService';
 export class SeatingArrangementComponent implements OnInit {
 
   public tablesList: Table[] = [];
-  // public SeatingArrangement: SeatingArrangement[] = [];
-  // constructor(private SeatingService: SeatingService,) { }
-  SeatingArrangemant: Table[];
+  public seatingArrangement: SeatingArrangement[];
+  constructor(private SeatingService: SeatingService, private GuestService: GuestService) { }
 
   ngOnInit(): void {
+
     //  this.SeatingService.getSeatingArrangemant().subscribe((data) =>{this.SeatingArrangemant=data;});
-    // this.SeatingService.getTablesList().subscribe((data) => { this.tablesList = data; });
-    // this.tablesList.forEach(table => {
-    //   // this.SeatingArrangement.push(Table = table, GuestsList =)
-    // });
-    this.setCircle()
+    this.SeatingService.getTablesList().subscribe((data) => {
+      this.tablesList = data;
+      this.fillGuests(data);
+      console.log('tablesList-in', this.tablesList);
+
+    });
+
+    // console.log('SeatingArrangemant', this.SeatingArrangemant);
+    //this.setCircle();
   }
 
+  fillGuests(tableList) {
+    this.seatingArrangement = [];
+    tableList.forEach(table => {
+      let guestList;
+      this.GuestService.getGuestsListByTableId(table.table_id).subscribe(data => {
+        guestList = data;
+        console.log('guestList', guestList);
+        this.seatingArrangement.push(new SeatingArrangement(table, guestList));
+        console.log('SeatingArrangement', this.seatingArrangement);
+        this.setCircle();
+      });
+    });
+  }
   setCircle() {
     document.querySelectorAll('.ciclegraph').forEach((ciclegraph) => {
       let circles = ciclegraph.querySelectorAll('.circle')
@@ -36,6 +55,13 @@ export class SeatingArrangementComponent implements OnInit {
         document.getElementById("circle" + i).style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth / 2}px) rotate(-${angle}deg)`
       }
     })
+  }
+
+  GetSeatings() {
+    console.log(this.seatingArrangement.length, 'aaaa');
+    console.log(this.seatingArrangement,"ghjk");
+    
+    return this.seatingArrangement !=null && this.seatingArrangement.length > 0;
   }
 
 }
